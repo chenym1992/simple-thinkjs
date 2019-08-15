a80f2abc35ab55b0e9d245698ca2d125e91ab3ee
chenym1992 <18251821296@163.com>
2019-08-16 03:27:23 +0800

    update

--- development.js
+++ development.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const Application = require('thinkjs');
-const watcher = require('think-watcher');
+const path = require("path");
+const Application = require("thinkjs");
+const watcher = require("think-watcher");
 
 const instance = new Application({
   ROOT_PATH: __dirname,
-  APP_PATH: path.join(__dirname, 'src'),
+  APP_PATH: path.join(__dirname, "src"),
   watcher: watcher,
-  env: 'development'
+  env: "development"
 });
 
 instance.run();
